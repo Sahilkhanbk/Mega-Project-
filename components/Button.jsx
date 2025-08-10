deb205012ef2b22d0d3d57c82b4c922629460d20
sahil khan <139511550+Sahilkhanbk@users.noreply.github.com>
2025-08-10 17:22:22 +0530

    Add files via upload

---
+++ components/Button.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Button(
+    { Children, type = "text", bgColor = "bg-blue-500", textColor = "text-White", className = "", ...props }) {
+    return (
+        <div>
+            <button className={`px-4 py-2 rounded-lg ${className} ${textColor} ${bgColor} `} {...props}> 
+                {Children}
+            </button>
+        </div>
+    )
+}
+
+export default Button
