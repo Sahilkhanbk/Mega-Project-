f70f4e118554b96426392e4f9feb9ab964fff697
sahil khan <139511550+Sahilkhanbk@users.noreply.github.com>
2025-08-12 22:51:08 +0530

    Update Index.js

--- components/Index.js
+++ components/Index.js
@@ -3,5 +3,12 @@ import Footer from "./Footer/Footer";
 import Container from './container/Container';
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import Input from "./Input";
+import Button from './Input'
+import RTE from "./RTE";
+import Signup from './Signup'
+import Login from './Login'
+import PostForm from './post-form/PostForm'
+import PostCard from './PostCard'
 
-export { Header, Footer, Container, Logo, LogoutBtn }
+export { Header, Footer, PostCard, PostForm, Container, Signup, Login, Logo, LogoutBtn, Input, Button, RTE }
