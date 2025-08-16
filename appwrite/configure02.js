82797b98ef731894ded5159f401df82fc698173f
sahil khan <139511550+Sahilkhanbk@users.noreply.github.com>
2025-08-16 16:17:12 +0530

    Update configure02.js

--- appwrite/configure02.js
+++ appwrite/configure02.js
@@ -1,6 +1,7 @@
 import conf from '../conf/conf.js';
 import { Client, ID, Databases, Storage, Query } from "appwrite";
 
+
 export class Service {
     client = new Client();
     databases;
@@ -141,6 +142,7 @@ export class Service {
 
 const service = new Service()
 export default service
+
 
 
 
